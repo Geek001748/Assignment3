d9e68d6fc7e721b9a497ecb3ba87afea1463ecdc
Geek001748 <arsen.serikbai01@gmail.com>
2024-05-06 19:52:55 +0500

    Initial methods

---
+++ IMyHashTable.java
@@ -0,0 +1,8 @@
+public interface IMyHashTable<K, V> {
+    int hash (K key);
+    void put (K key, V value);
+    V get (K key);
+    V remove (K key);
+    boolean contains(V value);
+    K getKey(V value);
+}
---
+++ MyHashTable.java
@@ -0,0 +1,65 @@
+public class MyHashTable <K,V> implements IMyHashTable{
+
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode (K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        @Override
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+    public MyHashTable(){
+         chainArray = new HashNode[M];
+    }
+    public MyHashTable(int M){
+         this.M = M;
+         chainArray = new HashNode[M];
+    }
+    @Override
+    public int hash(Object key) {
+        return key.hashCode() % M;
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+        int index = hash(key);
+        HashNode<K, V> newNode = new HashNode<>((K)key, (V)value);
+        if(chainArray[index] == null)
+        {
+            chainArray[index] = newNode;
+        }
+        else
+        {
+            HashNode<K, V> curr = chainArray[index];
+        }
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object getKey(Object value) {
+        return null;
+    }
+}
