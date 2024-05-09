d033d329e992adca8c06a79a9bb58926ee5a3f40
Geek001748 <arsen.serikbai01@gmail.com>
2024-05-09 19:44:46 +0500

    BST setup

--- BST.java
+++ BST.java
@@ -1,2 +1,43 @@
-public class BST {
+
+public class BST<K extends Comparable<K>, V> implements IBST<K, V>{
+
+    private Node root;
+    private int size;
+    public int BST()
+    {
+        this.root = null;
+        this.size = 0;
+    }
+    private class Node
+    {
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V value)
+        {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    @Override
+    public void put(K key, V val) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void delete(K key) {
+
+    }
+
+    @Override
+    public Iterable<K> iterator() {
+        return null;
+    }
 }
--- IBST.java
+++ IBST.java
@@ -2,4 +2,5 @@ public interface IBST<K,V>{
     void put (K key, V val);
     V get(K key);
     void delete(K key);
+    Iterable<K> iterator();
 }
